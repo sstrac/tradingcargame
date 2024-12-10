6b6cab7a7f910a046a4420ac16ff7a9ff983084f
Shannon-Strachan <Shannon.Strachan@autotrader.co.uk>
2024-12-10 19:49:45 +0000

    rotating player

--- player.gd
+++ player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody3D
 
 @onready var animation_player = get_node("AnimationPlayer")
+@onready var character = get_node("Character")
+
+var y_rotation
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+
+
+func _ready():
+	y_rotation = character.rotation.y
 
 
 func _physics_process(delta):
@@ -23,6 +30,27 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		var rotate_left = PI / 2
+		var rotate_right = -PI / 2
+		var rotate_down_clockwise = -PI
+		var rotate_down_anticlockwise = PI
+		var rotate_up = 0.0
+		
+		print(character.rotation.y)
+		if direction.z == -1:
+			y_rotation = rotate_up
+		elif direction.x == -1:
+			y_rotation = rotate_left
+		elif direction.z == 1 and character.rotation.y < 0:
+			y_rotation = rotate_down_clockwise
+		elif direction.z == 1 and character.rotation.y >= 0:
+			y_rotation = rotate_down_anticlockwise
+		elif direction.x == 1:
+			y_rotation = rotate_right
+		#else:
+			#y_rotation = y_rotation
+			
+		character.rotation.y = lerp(character.rotation.y, y_rotation, delta * 5)
 		animation_player.play("bob")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
