0d85f8ce40b80ce6dcbbf0d766ebac10b6cd8377
Daniel Woodruffe <daniel.woodruffe@autotrader.co.uk>
2024-12-10 14:49:15 +0000

    wip3

--- vehicle_metadata.gd
+++ vehicle_metadata.gd
@@ -2,6 +2,16 @@ extends Resource
 class_name VehicleMetadata
 
 @export var valuation: int
+@export var depricationFactor: float
+@export var fluctuationFactor: float
 
-func _init(valuation: int = 0) -> void:
+func _init(valuation: int = 0, depricationFactor: float = 0.9, fluctuationFactor: float = 0.05) -> void:
 	self.valuation = valuation
+	self.depricationFactor = depricationFactor
+	self.fluctuationFactor = fluctuationFactor
+	ValueChangingTimer.timeout.connect(updateValuation)
+
+func updateValuation():
+	print(valuation)
+	var valueAfterDeprication = valuation * randf_range(depricationFactor, 1)
+	self.valuation = valueAfterDeprication * randf_range((1+fluctuationFactor), (1-fluctuationFactor))
