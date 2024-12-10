739c85c71aec5092f5f98b2b894977178a118bf0
Shannon-Strachan <Shannon.Strachan@autotrader.co.uk>
2024-12-10 14:54:14 +0000

    joined

--- vehicle_database.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var vehicles: Array[VehicleMetadata] = []
-
-func _ready():
-	vehicles.append(VehicleMetadata.new(25000))
-	vehicles.append(VehicleMetadata.new(35000))
-	vehicles.append(VehicleMetadata.new(20000))
-	vehicles.append(VehicleMetadata.new(12500))
