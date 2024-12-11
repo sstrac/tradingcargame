43c554c59c672b5a26cec2d16a72ec58219210fb
Daniel Woodruffe <daniel.woodruffe@autotrader.co.uk>
2024-12-11 15:10:19 +0000

    updated autotrader dynamic values

--- autotrader/adverts_panel.gd
+++ autotrader/adverts_panel.gd
@@ -6,15 +6,21 @@ extends PanelContainer
 @onready var button = get_node("AdvertHBox/Button")
 
 var metadata
+var bid
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	metadata.valuationUpdated.connect(_updateText)
 	vehicle_label.set_text("{Vehicle name}")
-	valuation_label.set_text("%s" % metadata.valuation)
-	offer_label.set_text("{offer}")
+	valuation_label.set_text("£%s" % metadata.valuation)
+	offer_label.set_text("£%s" % bid.offer_price)
+
 
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _updateText():
+	valuation_label.set_text("£%s" % metadata.valuation)
--- autotrader/autotrader.gd
+++ autotrader/autotrader.gd
@@ -4,6 +4,9 @@ var advert = preload("res://autotrader/adverts_panel.tscn")
 
 func populate_with_forecourt_vehicles():
 	for vehicle in VehicleDatabase.vehicles_on_forecourt:
+		var bid = BidMetadata.new(vehicle)
+		Bids.bids.append(bid)
 		var vehicleAd = advert.instantiate()
 		vehicleAd.metadata = vehicle
+		vehicleAd.bid = bid
 		add_child(vehicleAd)
